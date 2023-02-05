16726c9031c2c1f36eebe89b45bba84caa5defc0
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-02-05 16:06:08 +0900

    New: testcase > test

---
+++ testcase/test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * op-unit-app:/testcase/test.php
+ *
+ * @creation  2019-04-02
+ * @version   1.0
+ * @package   op-unit-app
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+D('Test is successful!');
