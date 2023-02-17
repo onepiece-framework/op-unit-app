84b1d19b72e5a4c23821ead7f764860077c0168d
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-02-17 19:38:30 +0900

    Fix: ci.php | v2.1

--- ci.php
+++ ci.php
@@ -2,7 +2,7 @@
 /** op-core:/ci.php
  *
  * @created    2023-01-01
- * @version    2.0
+ * @version    2.1
  * @package    op-core
  * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright  Tomoaki Nagahara All right reserved.
