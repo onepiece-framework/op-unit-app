d92750f8318730ac50ad94f55a7287f535ab4d46
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-12-28 02:33:41 +0900

    2023: APP | PHPDoc

--- APP.trait.php
+++ APP.trait.php
@@ -1,6 +1,5 @@
 <?php
-/**
- * op-unit-app:/APP.trait.php
+/** op-unit-app:/APP.trait.php
  *
  * @created   2019-11-28
  * @version   1.0
@@ -8,20 +7,19 @@
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
 
 /** namespace
  *
- * @created   2019-11-28
  */
 namespace OP;
 
-/** APP
+/** UNIT_APP
  *
- * @created   2019-11-28
- * @version   1.0
- * @package   op-unit-app
- * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
- * @copyright Tomoaki Nagahara All right reserved.
  */
 trait UNIT_APP
 {
